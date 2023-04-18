d47324526f5665f143e970b2f14ca3345fe57362
Nickson K Rop <nickyrutto@gmail.com>
2023-04-17 22:00:53 -0700

    header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define TOKEN_DELIM " \t\r\n\a"
+#define BUFFER_SIZE 1024
+
+
+#endif
