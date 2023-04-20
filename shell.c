31c4514e616af596dcf2dffa2edb5defefbc2ece
Nickson K Rop <nickyrutto@gmail.com>
2023-04-20 03:16:43 -0700

    main function

---
+++ shell.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+  * main - execute the shell
+  * Return: 0 on success
+  */
+int main(void)
+{
+	char *buffer = malloc(BUFFER_SIZE * (sizeof(char)));
+	char *args[BUFFER_SIZE];
+	int wait_status;
+
+	while (1)
+	{
+		printf("$ ");
+		fgets(buffer, BUFFER_SIZE, stdin);
+
+		char *token = strtok(buffer, TOKEN_DELIMITER);
+		int i = 0;
+
+		while (token != NULL)
+		{
+			args[i++] = token;
+			token = strtok(NULL, TOKEN_DELIMITER);
+		}
+		args[i] = NULL;
+
+		pid_t pid = fork();
+
+		if (pid == 0)
+		{
+			execvp(args[0], args);
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+		}
+		else
+		{
+			my_waitpid(pid, &wait_status, WUNTRACED);
+		}
+	}
+	free(buffer);
+	return (0);
+}
