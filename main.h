7aa3d3b4bb415f3d25571c52d28a0db0d5c78651
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-26 13:43:01 -0700

    update

--- main.h
+++ main.h
@@ -12,6 +12,8 @@
 #define TOKEN_DELIMITER " \t\r\n\a"
 #define BUFFER_SIZE 1024
 
+typedef struct program_data program_data;
+
 char *my_fgets(char *s, int size, FILE *stream);
 char *my_strtok(char *str, const char *delim);
 pid_t my_waitpid(pid_t pid, int *wstatus, int options);
@@ -19,6 +21,5 @@ pid_t my_fork(void);
 int my_execvp(char *file, char **args);
 void type_prompt(void);
 void read_command(char cmd[], char *par[]);
-
-
+int execute_command(program_data *prog_data);
 #endif
