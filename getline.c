0816372d3219b8a9d22a746e966ba43fcb55c362
Nickson K Rop <nickyrutto@gmail.com>
2023-04-26 11:35:00 -0700

    getline

---
+++ getline.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream) {
+    char *line = NULL;
+    size_t size = 0;
+    ssize_t len = 0;
+    int c;
+
+    while ((c = fgetc(stream)) != EOF) {
+        if (size <= len + 1) {
+            size += 256;
+            line = realloc(line, size);
+            if (!line) {
+                return -1;
+            }
+        }
+
+        line[len++] = c;
+
+        if (c == '\n') {
+            break;
+        }
+    }
+
+    if (len == 0) {
+        return -1;
+    }
+
+    line[len] = '\0';
+
+    if (*lineptr) {
+        free(*lineptr);
+    }
+
+    *lineptr = line;
+    *n = size;
+
+    return len;
+}
